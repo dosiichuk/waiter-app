75fa8e11054adb6567d65a7b7ecb598db9e49b41
Dmytro <dmytro.osiychuk@gmail.com>
2022-02-16 16:18:49 +0100

    Add spinner to homepage

--- src/components/features/Tables/Tables.js
+++ src/components/features/Tables/Tables.js
@@ -1,12 +1,19 @@
 import { useSelector } from 'react-redux';
 import ListGroup from 'react-bootstrap/ListGroup';
+import { Spinner } from 'react-bootstrap';
 import { getAllTables } from '../../../redux/tablesRedux';
 import TableListItem from '../TableListItem/TableListItem';
 
 const Tables = () => {
   const tables = useSelector(getAllTables);
+
   return (
     <ListGroup>
+      {tables.length === 0 && (
+        <Spinner animation="border" role="status" className="m-auto">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      )}
       {tables.map((table) => (
         <TableListItem key={table.id} {...table} />
       ))}
